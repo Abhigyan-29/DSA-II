352eedc200d33579a2c668a8a3c7949b88546ae1
Abhigyan Prakash <prakashabhigyan2002@gmail.com>
2024-08-27 01:54:30 +0530

    Reverse a stack using recursion

---
+++ Stacks/ReverseRecursive.java
@@ -0,0 +1,25 @@
+package Stacks;
+
+import java.util.Stack;
+
+public class ReverseRecursive {
+    public static void rec(Stack<Integer>  st){
+        if(st.size() == 0){
+            return;
+        }
+        int top = st.pop();
+        System.out.println(top+" ");
+        rec(st);
+        st.push(top);
+    }
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack();
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        st.push(5);
+        rec(st);
+
+    }
+}
